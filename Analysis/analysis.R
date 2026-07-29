fc3f059ae0ad2f1f5d60178444932d296a3f1107
Carol Xu <xucarol.20@gmail.com>
2026-07-28 21:07:39 -0400

    Initial Commit

---
+++ Analysis/analysis.R
@@ -0,0 +1,10 @@
+# Set working directory
+setwd("C:/Users/CarolXu/OneDrive - Cato Institute/Desktop/Intern Coding Lab")
+
+# Packages
+install.packages("tidyverse")
+library(tidyverse)
+
+# load the data
+data <- read_csv("data/acs00014.csv")
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Cato Intern Coding Lab 2026
+## 30 June 2026
+
+> Working with the American Community Survey 
+
+** Instructions and Code **
+https://carol-xu4.github.io/Cato-Intern-Coding_lab/
+
+** Download dataset here **
